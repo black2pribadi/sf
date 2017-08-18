c4547931b816e167e83ba1c6f8c613d48bd565b3
black2pribadi <30944511+black2pribadi@users.noreply.github.com>
2017-08-18 07:52:06 -0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -53,7 +53,7 @@ KODE IKLAN DISINI
 <b>Your link show here</b> <i aria-hidden="true" class="fa fa-hand-o-down"></i></h3>
 </div>
 <div class="panel-body">
-<script src="https://cdn.rawgit.com/black2pribadi/sf/d3822ced/safelink.js" type="text/javascript"></script>
+<script src="https://rawgit.com/black2pribadi/sf/master/safelink.js" type="text/javascript"></script>
 <script type="text/javascript">
 var currentURL=location.href;
 var str = currentURL;
